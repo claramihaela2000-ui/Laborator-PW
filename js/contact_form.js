f3006419dd8661028d3ec990bc15e6367de2afe3
Chitu Clara <claramihaela2000@gmail.com>
2026-02-25 11:08:44 +0200

    Laboratorul 1

---
+++ js/contact_form.js
@@ -0,0 +1,12 @@
+function submitForm() {
+
+    const nume = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
+    const mesaj = document.getElementById("message").value;
+
+    console.log("Nume:", nume);
+    console.log("Email:", email);
+    console.log("Mesaj:", mesaj);
+
+    console.warn("Goodbye World!");
+}

00f6a13645087d2431b57baafa6bfd37256510a3
Chitu Clara <claramihaela2000@gmail.com>
2026-03-18 11:43:11 +0200

    feat: React and Vite setup

---
+++ src/Card.jsx
@@ -0,0 +1,11 @@
+function Card(props) {
+    return (
+        <div>
+            <p> True Diva </p>
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+
+        </div>
+    );
+}
+export default Card;
